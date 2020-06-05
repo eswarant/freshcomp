c3eaaff4e87bb1c9d9f9bf6535888bd999362c07
eswaran thirumalairaj <rteswaran@gmail.com>
2020-06-05 11:54:37 +0530

    Updates

---
+++ precache-manifest.6efac18b3051b3051def538a783cc442.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ce5c9ca7749aa3efc6920af06818689",
+    "url": "./index.html"
+  },
+  {
+    "revision": "8a05c37c54daf4625560",
+    "url": "./static/css/main.b693b373.chunk.css"
+  },
+  {
+    "revision": "a4552e9c90924edd1890",
+    "url": "./static/js/2.2087423b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.2087423b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a05c37c54daf4625560",
+    "url": "./static/js/main.f809a146.chunk.js"
+  },
+  {
+    "revision": "ced7be9129f4bc690729",
+    "url": "./static/js/runtime-main.f64315a7.js"
+  }
+]);
